973e31d72ca7dab5e7b5690fb164fdfe8779c9ea
Li-plus <liplus17@163.com>
2019-08-10 20:41:46 +0800

    remove all qmui android:textColor ref

--- app/src/main/java/com/inftyloop/indulger/fragment/MeFragment.java
+++ app/src/main/java/com/inftyloop/indulger/fragment/MeFragment.java
@@ -5,20 +5,27 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 import com.inftyloop.indulger.R;
 import com.qmuiteam.qmui.arch.QMUIFragment;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
 import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView;
 
 public class MeFragment extends QMUIFragment {
     private static final String TAG = MeFragment.class.getSimpleName();
 
-    @BindView(R.id.topbar) QMUITopBarLayout mTopBar;
-    @BindView(R.id.avatar) ImageView mAvatar;
-    @BindView(R.id.username) TextView mUsername;
-    @BindView(R.id.me_list) QMUIGroupListView mGroupListView;
+    @BindView(R.id.topbar)
+    QMUITopBarLayout mTopBar;
+    @BindView(R.id.avatar)
+    ImageView mAvatar;
+    @BindView(R.id.username)
+    TextView mUsername;
+    @BindView(R.id.me_list)
+    QMUIGroupListView mGroupListView;
 
     @Override
     public View onCreateView() {
@@ -26,11 +33,14 @@ public class MeFragment extends QMUIFragment {
         ButterKnife.bind(this, root);
         mTopBar.setTitle(getResources().getString(R.string.me_title));
         mUsername.setText("Username Placeholder"); // TODO
+
+        QMUICommonListItemView itemSettings = mGroupListView.createItemView(getString(R.string.me_grouplist_settings));
+        itemSettings.setAccessoryType(QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON);
         QMUIGroupListView.newSection(getContext())
-                .addItemView(mGroupListView.createItemView(getResources().getString(R.string.me_grouplist_settings)), v -> {
-                        QMUIFragment fragment = new SettingsFragment();
-                        startFragment(fragment);
-                    }).addTo(mGroupListView);
+                .addItemView(itemSettings, (View v) -> {
+                    QMUIFragment fragment = new SettingsFragment();
+                    startFragment(fragment);
+                }).addTo(mGroupListView);
         return root;
     }
 
