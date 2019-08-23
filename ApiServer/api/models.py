b0e813a1ac8933b0015732fcd58a5d06a3abde3e
zx1239856 <zx1239856@gmail.com>
2019-08-24 00:00:51 +0800

    Enforce unique constraint in APIServer

--- ApiServer/api/models.py
+++ ApiServer/api/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(db_index=True, max_length=100)
+    username = models.CharField(db_index=True, max_length=100, unique=True)
     password = models.CharField(max_length=100)
     email = models.EmailField()
     register_date = models.DateTimeField(auto_now_add=True)
 
 class JsonData(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    varname = models.CharField(db_index=True, max_length=100)
+    varname = models.CharField(db_index=True, max_length=100, unique=True)
     json = models.TextField()
